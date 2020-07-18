3b2a4c17763bb8c8f721ed306cd7f216225f3ac5
Laura Collins <laura.e.collins1@gmail.com>
2020-07-18 12:48:27 -0500

    registration done

--- takugaku.ui/src/App/App.js
+++ takugaku.ui/src/App/App.js
@@ -12,6 +12,7 @@ import './App.scss';
 import Home from '../components/pages/Home/Home';
 import SchoolForm from '../components/pages/SchoolForm/SchoolForm';
 import firebaseApp from '../helpers/data/connection';
+import schoolData from '../helpers/data/schoolData';
 
 const PrivateRoute = ({ component: Component, authed, ...rest }) => {
   const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
@@ -23,16 +24,35 @@ firebaseApp();
 class App extends React.Component {
   state = {
     authed: false,
+    uid: '',
+    school: {},
+    schoolExists: false,
   }
 
   componentDidMount() {
     this.removeListener = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.setState({ authed: true });
+        this.setState({ uid: user.uid });
+        schoolData.getSchoolByUid(user.uid)
+          .then((response) => {
+            this.setState({ school: response });
+            this.setState({ schoolExists: true });
+          })
+          .catch(() => {
+            this.setState({ school: {} });
+          });
       } else {
         this.setState({ authed: false });
+        this.setState({ uid: '' });
+        this.setState({ school: {} });
+        this.setState({ schoolExists: false });
       }
     });
+  }
+
+  setSchool = (schoolData) => {
+    this.setState({ school: schoolData });
   }
 
   componentWillUnmount() {
@@ -40,14 +60,19 @@ class App extends React.Component {
   }
 
   render() {
-    const { authed } = this.state;
+    const {
+      authed,
+      uid,
+      school,
+      schoolExists,
+    } = this.state;
 
     return (
       <div className="App">
         <Router>
           <Switch>
-            <Route path="/" exact render={(props) => <Home {...props} authed={authed}/>} />
-            <Route path="/register/school" component={SchoolForm}/>
+            <Route path="/" exact render={(props) => <Home {...props} authed={authed} uid={uid} school={school} />} />
+            <Route path="/register/school" render={(props) => <SchoolForm {...props} authed={authed} uid={uid} school={school} schoolExists={schoolExists} setSchool={this.setSchool}/>} />
           </Switch>
         </Router>
       </div>
