15feb69449f5f06cae5d759f366e9f84f432dda6
Laura Collins <laura.e.collins1@gmail.com>
2020-07-18 16:52:17 -0500

    teacher login info pulling from DB

--- takugaku.ui/src/components/pages/TeacherLogIn/TeacherLogIn.js
+++ takugaku.ui/src/components/pages/TeacherLogIn/TeacherLogIn.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import owl from '../Home/images/owl.png';
-// import teacherData from '../../../helpers/data/teacherData';
+import teacherData from '../../../helpers/data/teacherData';
 
 class TeacherLogIn extends React.Component {
     state = {
@@ -17,62 +17,78 @@ class TeacherLogIn extends React.Component {
       this.setState({ userName: e.target.value });
     }
 
-      pinChange = (e) => {
-        e.preventDefault();
-        this.setState({ pin: e.target.value });
+    pinChange = (e) => {
+      e.preventDefault();
+      this.setState({ pin: e.target.value });
+    }
+
+    logInTeacher = (e) => {
+      e.preventDefault();
+      const { schoolId } = this.props.school;
+      if (this.state.pin.length !== 4) {
+        this.setState({ invalidPin: true });
+      } else {
+        this.setState({ invalidPin: false });
+        const pin = parseInt(this.state.pin, 10);
+        teacherData.teacherValidation(schoolId, this.state.userName, pin)
+          .then((response) => {
+            console.error(response);
+          })
+          .catch((error) => console.error(error, 'error from login teacher'));
       }
+    }
 
-      render() {
-        const { teacherExists } = this.props;
-        const { userName, pin, invalidPin } = this.state;
+    render() {
+      const { teacherExists } = this.props;
+      const { userName, pin, invalidPin } = this.state;
 
-        return (
-            <div className="TeacherLogIn">
-                { !teacherExists ? (<Redirect push to={{ pathname: '/register/teacher' }} />)
-                  : ('')}
-                <h1>Teacher Log In</h1>
-                <img id="owl" src={owl} alt="owl"/>
-                <div className="container">
-                <form className="formContainer" onSubmit={this.logInTeacher}>
-                <div className="form-inline d-flex justify-content-center">
-                    <div className="form-group row">
-                    <label htmlFor="userName" className="col-form-label">User Name:</label>
-                    <input
-                    type="text"
-                    className="form-control m-2"
-                    id="userName"
-                    placeholder="Enter User Name"
-                    value={userName}
-                    onChange={this.userNameChange}
-                    required
-                    >
-                    </input>
-                    </div>
+      return (
+        <div className="TeacherLogIn">
+            { !teacherExists ? (<Redirect push to={{ pathname: '/register/teacher' }} />)
+              : ('')}
+            <h1>Teacher Log In</h1>
+            <img id="owl" src={owl} alt="owl"/>
+            <div className="container">
+            <form className="formContainer" onSubmit={this.logInTeacher}>
+            <div className="form-inline d-flex justify-content-center">
+                <div className="form-group row">
+                <label htmlFor="userName" className="col-form-label">User Name:</label>
+                <input
+                type="text"
+                className="form-control m-2"
+                id="userName"
+                placeholder="Enter User Name"
+                value={userName}
+                onChange={this.userNameChange}
+                required
+                >
+                </input>
                 </div>
-                <div className="form-inline d-flex justify-content-center">
-                    <div className="form-group row">
-                    <label htmlFor="PIN" className="col-form-label">Please Enter a 4-Digit PIN:</label>
-                    <input
-                    type="number"
-                    className="form-control m-2"
-                    id="PIN"
-                    value={pin}
-                    onChange={this.pinChange}
-                    required
-                    >
-                    </input>
-                    </div>
-                </div>
-                { invalidPin ? (<div className="warning">Please enter a valid PIN!</div>)
-                  : ('')}
-                <div className="buttonContainer">
-                    <Button variant="secondary" className="formButton" type="submit">Log In</Button>
-                </div>
-                </form>
             </div>
+            <div className="form-inline d-flex justify-content-center">
+                <div className="form-group row">
+                <label htmlFor="PIN" className="col-form-label">Please Enter a 4-Digit PIN:</label>
+                <input
+                type="number"
+                className="form-control m-2"
+                id="PIN"
+                value={pin}
+                onChange={this.pinChange}
+                required
+                >
+                </input>
+                </div>
+            </div>
+            { invalidPin ? (<div className="warning">Please enter a valid PIN!</div>)
+              : ('')}
+            <div className="buttonContainer">
+                <Button variant="secondary" className="formButton" type="submit">Log In</Button>
+            </div>
+            </form>
         </div>
-        );
-      }
+    </div>
+      );
+    }
 }
 
 export default TeacherLogIn;
--- takugaku.ui/src/helpers/data/teacherData.js
+++ takugaku.ui/src/helpers/data/teacherData.js
@@ -6,13 +6,23 @@ const { baseUrl } = apiKeys.baseUrl;
 const getTeacherBySchoolId = (schoolId) => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/api/takugaku/teacher/schoolId/${schoolId}`)
     .then((result) => {
-      const school = result.data;
+      const teacher = result.data;
 
-      resolve(school);
+      resolve(teacher);
+    })
+    .catch((error) => reject(error));
+});
+
+const teacherValidation = (schoolId, userName, pin) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/api/takugaku/teacher/schoolId/${schoolId}/username/${userName}/pin/${pin}`)
+    .then((result) => {
+      const teacher = result.data;
+
+      resolve(teacher);
     })
     .catch((error) => reject(error));
 });
 
 const registerTeacher = (newTeacher) => axios.post(`${baseUrl}/api/takugaku/teacher`, newTeacher);
 
-export default { getTeacherBySchoolId, registerTeacher };
+export default { getTeacherBySchoolId, registerTeacher, teacherValidation };
