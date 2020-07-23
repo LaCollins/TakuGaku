488f78d9ad16dd61edfff193948b55683adb3042
Laura Collins <laura.e.collins1@gmail.com>
2020-07-22 19:19:31 -0500

    api installed

---
+++ takugaku.ui/src/components/shared/Calendar/Calendar.js
@@ -0,0 +1,12 @@
+import React, { ReactNode, SyntheticEvent } from 'react';
+import ApiCalendar from 'react-google-calendar-api';
+import apiCredentials from '../../../helpers/credentials.json';
+
+class Calendar extends React.Component {
+  render() {
+    return (
+            <div className="Calendar">
+            </div>
+    );
+  }
+}
